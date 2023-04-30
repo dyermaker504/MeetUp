5eaa3f9ba992383fdd7ee377559f357ef46941d3
ClayLewis2 <113313020+ClayLewis2@users.noreply.github.com>
2023-04-30 14:24:11 -0500

    Integrate Login and Registration

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3, requests, json, googlemaps, numpy as np, sys, urllib.parse
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, flash, g, current_app, redirect, url_for
 from datetime import datetime
 
 def get_db_connection():
@@ -94,6 +94,7 @@ def placesearch():
     nearby_results_for_marker = []
     for results in results:
         googlesearch = results.get('name') + ' ' + results.get('vicinity')
+        googlesearch = urllib.parse.quote_plus(googlesearch)
         nearby_results_filtered.append([results.get('name'), results.get('vicinity'), results.get('rating'), googlesearch])
         resultsgeo = results.get('geometry')
         resultsgeo = resultsgeo.get('location')
@@ -102,130 +103,18 @@ def placesearch():
     return render_template('index.html', friends = friends, nearby_results = nearby_results_filtered, center = center_coords, selectedlocations = locationswithuser, returnedlocations = nearby_results_for_marker)
 
 def get_friends_list():
-    dbconnect = get_db_connection()
-    friends = dbconnect.execute('SELECT username, address, latitude, longitude FROM users').fetchall()
-    dbconnect.close()
+    if 'user_id' in session:
+        dbconnect = get_db_connection()
+        friends = dbconnect.execute('SELECT users.username, users.address, latitude, longitude FROM users INNER JOIN friends ON users.id = friends.friend_id WHERE users.username = ? OR friends.user_id = ? ORDER BY users.username ASC', (session['user_id'],session['user_id'])).fetchall()
+        dbconnect.close()
+    else:
+        friends = ''
     return friends
 
 # This function will be called when a 500 error occurs
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('error.html', error=error), 500
-
-#get data from homepage and send to result.html
-@app.route('/result', methods=['POST'])
-def process_input():
-    currently_open = True #look for items that are open can be set in webpage later
-    keyword = request.form['keyword']
-    address1 = request.form['address1']
-    address2 = request.form['address2']
-    address3 = request.form['address3']
-    address4 = request.form['address4']
-    address5 = request.form['address5']
-    address6 = request.form['address6']
-    address7 = request.form['address7']
-    address7 = request.form['address8']
-    # Initialize an empty list to store the addresses
-    #addresses_list = [] #list for address inputs
-    addresses_coords = [] #list of coord pairs of the addresses
-    # Loop through each address
-    for i in range(1, 9):
-        address_name = f"address{i}"  # Assumes address names are address1, address2, etc.
-        address_value = request.form.get(address_name)  # Assumes you are using Flask or similar framework
-        # Check if the address has a value
-        if address_value:
-            # If the address has a value, add it to the list
-            geocoded_address = gmaps.geocode(address_value)
-            addresses_coords.append(get_coords(geocoded_address))
-            #addresses_list.append(address_value)
-    
-    #get the center coords of the addresses
-    center_coords = get_geocenter(addresses_coords)
-    print("Coords: ", center_coords)
-    print("Type: ",type(center_coords))
-    #set radius each time it's called
-    results_min = 10  #how many results
-    min_radius = 300 #meters
-    max_radius = 60000 #largest it will search
-    search_radius = min_radius
-    radius_grow_rate = 1 
-    radius_grow_rate_min = 0.1 #for rate scaling
-    safety_counter = 1
-    
-    search_success = False
-    while not search_success:
-        if safety_counter > 20:
-            print("Something's wrong with the loop.")
-            break
-        nearby_results = gmaps.places_nearby(center_coords, search_radius, keyword = keyword, open_now=currently_open)
-        #check the status
-        query_status = nearby_results['status']   
-        if search_radius >= max_radius:
-            #try an error
-            raise Exception("Max search radius exceeded")
-            break #out of the
-        elif query_status == "ZERO_RESULTS":
-            search_radius += search_radius*radius_grow_rate
-        else:
-            results = nearby_results.get('results')
-            results_cnt = len(results)
-            #check how many results were returned
-            if results_cnt < results_min: #not enough results, grow radius
-                search_radius += search_radius*radius_grow_rate
-            else: #we have have enough
-                search_success = True
-        #scaling radius rate is working
-        if radius_grow_rate > radius_grow_rate_min: 
-            radius_grow_rate -= radius_grow_rate/10  #dynamically shrink rate as radius grows larger
-            #print("radius grow rate ",radius_grow_rate, "rate min ", radius_grow_rate_min)
-        print(safety_counter)
-        safety_counter += 1
-    #end of while
-
-
-
-    #for weekday index
-    weekday_names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    #get hours by calling places(), and adding to results[]
-    getfields = ['current_opening_hours','opening_hours']
-    for x in range(len(results)):
-        results_place_id = results[x]['place_id']
-        details = gmaps.place(results_place_id, session_token=None, fields=getfields)
-        #print currently open yes or no
-        currently_open = details['result']['opening_hours']['open_now']
-        opening_hours = details['result']['current_opening_hours']
-        current_day = datetime.today().weekday()
-        #print todays hours - what we want
-        todays_hours = opening_hours['weekday_text'][current_day]
-        results[x]['hours'] = todays_hours
-        # Get the name of the current day
-        #current_day_name = weekday_names[current_day]
-        # Print the opening hours for today with the day of the week
-        #print(current_day_name + "'s hours: ", todays_hours,"\n\n")       
-    
-    #trying to get map working but its giving a 'staticmaperror' on the webpage
-    # Generate the URL for the static map
- #   for coords in center_coords:
-#        str_center_coords = ', '.join(str(coords))
-    str_center_coords = ', '.join('{:.6f}'.format(coord) for coord in center_coords)
-    print(str_center_coords)
-    params = {
-        'center': str_center_coords,
-        'zoom': '14',
-        'size': '640x400',
-        'maptype': 'roadmap',
-        'key': api_key
-    }
-    print("Params: ",params)
-    #works with string
-    url = 'https://maps.googleapis.com/maps/api/staticmap?' + urllib.parse.urlencode(params)
-
-    #return render_template('result.html', results=results, details=hours)
-    return render_template('result.html', results=results,
-                           center_coords=center_coords,
-                           keyword=keyword,
-                           static_map_url=url)
-
 
 def get_geocenter(coord_list):
   #Takes a coord pair list
@@ -248,3 +137,136 @@ def get_coords(geo_address):
 #Client API key
 api_key = "AIzaSyCQHMg2sZwnUTRQavAXTYhqv7BGzl7j2EQ"
 gmaps = googlemaps.Client(key=api_key)
+
+app.secret_key = 'secretwords'
+
+# dbconnectect to database
+def get_db_dbconnectection():
+    dbconnect = sqlite3.connect('friendfinder.db')
+    dbconnect.row_factory = sqlite3.Row
+    return dbconnect
+
+# Registration form route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        address = request.form['address']
+        password = request.form['password']
+        
+        googlelatlng = gmaps.geocode(address=address)
+        googlelatlng = googlelatlng[0].get('geometry')
+        googlelatlng = googlelatlng.get('location')
+        googlelatlng = [googlelatlng['lat'],googlelatlng['lng']]
+
+        #Here I figure we convert address to coords and save in users, for long,lat
+        #didn't wannna make the requests during testing here
+
+        # Check if username already exists
+        dbconnect = get_db_dbconnectection()
+        user = dbconnect.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+        if user is not None:
+            return 'Username already exists!'
+
+        # Insert new user into database
+        dbconnect.execute('INSERT INTO users (username, address, password, latitude, longitude) VALUES (?, ?, ?, ?, ?)', (username, address, password, googlelatlng[0], googlelatlng[1]))
+        dbconnect.commit()
+        dbconnect.close()
+        
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+# Login form route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if email and password are correct
+        dbconnect = get_db_dbconnectection()
+        user = dbconnect.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password)).fetchone()
+        dbconnect.close()
+        if user is None:
+           return redirect(url_for('register'))
+
+        # Save user id to session
+        session['user_id'] = user['id']
+        return redirect(url_for('dashboard'))
+
+    return render_template('login.html')
+
+# Dashboard route
+@app.route('/dashboard')
+def dashboard():
+    # Check if user is logged in
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    # Get user info from database
+    dbconnect = get_db_dbconnectection()
+    user = dbconnect.execute('SELECT * FROM users WHERE id = ?', (session['user_id'],)).fetchone()
+    
+    friends = get_friends_list()
+    
+    return render_template('dashboard.html', user=user, friends=friends)
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+@app.route('/add_friend', methods=['POST'])
+def add_friend():
+    # Check if user is logged in
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    # Get form data
+    friend_username = request.form.get('friend_username')
+
+    # Find friend user by username
+    dbconnect = get_db_dbconnectection()
+    friend_user = dbconnect.execute('SELECT * FROM users WHERE username = ?', (friend_username,)).fetchone()
+
+    # If friend user not found, display error message
+    if friend_user is None:
+        flash('User not found')
+        return redirect(url_for('dashboard'))
+
+    # Insert friendship into database
+    dbconnect.execute('INSERT INTO friends (user_id, friend_id) VALUES (?, ?)', (session['user_id'], friend_user['id']))
+    dbconnect.commit()
+    dbconnect.close()
+
+    # Display success message
+    flash('Friend added')
+    return redirect(url_for('dashboard'))
+
+@app.route('/change_address', methods=['POST'])
+def change_address():
+    # Check if user is logged in
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    # Get form data
+    address = request.form.get('change_address')
+    
+    #Get Geocode
+    googlelatlng = gmaps.geocode(address=address)
+    googlelatlng = googlelatlng[0].get('geometry')
+    googlelatlng = googlelatlng.get('location')
+    googlelatlng = [googlelatlng['lat'],googlelatlng['lng']]
+
+
+    # Find friend user by username
+    dbconnect = get_db_dbconnectection()
+    print("UPDATE users SET address='%s', latitude='%s', longitude='%s'WHERE id='%s'" % (address, googlelatlng[0], googlelatlng[1], session['user_id']))
+    dbconnect.execute("UPDATE users SET address='%s', latitude='%s', longitude='%s'WHERE id='%s'" % (address, googlelatlng[0], googlelatlng[1], session['user_id']))
+    dbconnect.commit()
+    dbconnect.close()
+
+    # Display success message
+    flash('Address Changed')
+    return redirect(url_for('dashboard'))
